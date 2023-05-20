f824868f161143be1e12d4ae84650f82e1c2c967
Protocol Corporation <protocollive@outlook.com>
2023-05-19 23:24:13 -0300

    Changed system class detection

--- src/StbObjects/StbModuleTools.php
+++ src/StbObjects/StbModuleTools.php
@@ -1,24 +1,16 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2023.02.02.02
+//2023.05.19.00
 
 namespace ProtocolLive\SimpleTelegramBot\StbObjects;
-use ProtocolLive\SimpleTelegramBot\StbObjects\{
-  StbAdmin,
-  StbAdminModules
-};
 
 abstract class StbModuleTools{
   public static function Load(
     string $Module
   ):void{
-    if(in_array($Module, self::System()) === false):
+    if(strpos($Module, 'ProtocolLive\SimpleTelegramBot') === false):
       require(DirModules . '/' . basename($Module) . '/index.php');
     endif;
-  }
-
-  private static function System():array{
-    return [StbAdmin::class, StbAdminModules::class];
   }
 }
