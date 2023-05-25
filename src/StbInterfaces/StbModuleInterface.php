87fc92d00b694d73c6fe8f6a73b6056eba4481c8
Protocol Corporation <protocollive@outlook.com>
2023-05-24 21:33:39 -0300

    Added Command

--- src/StbInterfaces/StbModuleInterface.php
+++ src/StbInterfaces/StbModuleInterface.php
@@ -5,12 +5,11 @@
 namespace ProtocolLive\SimpleTelegramBot\StbInterfaces;
 
 /**
- * @version 2023.05.23.00
+ * @version 2023.05.24.00
  */
 interface StbModuleInterface{
+  public static function Command():void;
   public static function Install():void;
-
   public static function Listener():void;
-
   public static function Uninstall():void;
 }
