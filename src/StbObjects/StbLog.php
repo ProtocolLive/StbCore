dbb6cca6c66a7c000e8aacdcf748e9bcbc4918a0
Protocol Corporation <protocollive@outlook.com>
2024-01-15 00:04:03 -0300

    Changing values

--- src/StbObjects/StbLog.php
+++ src/StbObjects/StbLog.php
@@ -1,11 +1,11 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
-//2023.05.14.00
+//2024.01.14.00
 
 namespace ProtocolLive\SimpleTelegramBot\StbObjects;
 
 abstract class StbLog{
-  const Cron = 16;
-  const Trace = 32;
+  const Cron = 32;
+  const Trace = 64;
 }
