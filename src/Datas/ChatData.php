2619646df93a3ee7b513da5b07c3f2e905fd9506
Protocol Corporation <protocollive@outlook.com>
2024-07-25 11:38:28 -0300

    Readonly class

--- src/Datas/ChatData.php
+++ src/Datas/ChatData.php
@@ -6,31 +6,23 @@ namespace ProtocolLive\SimpleTelegramBot\Datas;
 use ProtocolLive\SimpleTelegramBot\NoStr\Fields\Chats;
 
 /**
- * @version 2023.05.25.00
+ * @version 2024.07.24.00
  */
-final class ChatData{
-  public readonly int $Id;
-  public readonly string $Name;
-  public readonly string|null $NameLast;
-  public readonly string|null $Nick;
-  public readonly string|null $Language;
-  public readonly int $Permission;
-  public readonly int $Created;
-  public readonly int $LastSeen;
+final readonly class ChatData{
+  public int $Id;
+  public string $Name;
+  public string|null $NameLast;
+  public string|null $Nick;
+  public string|null $Language;
+  public int $Permission;
+  public int $Created;
+  public int $LastSeen;
 
-  public function __construct(array $Data){
+  public function __construct(
+    array $Data
+  ){
     foreach(Chats::cases() as $field):
       $this->{$field->name} = $Data[$field->value];
     endforeach;
-    /*
-    $this->Id = $Data[Chats::Id->value];
-    $this->Created = $Data[Chats::Created->value];
-    $this->Perms = $Data[Chats::Permission->value];
-    $this->Name = $Data[Chats::Name->value];
-    $this->NameLast = $Data[Chats::NameLast->value];
-    $this->Nick = $Data[Chats::Nick->value];
-    $this->Language = $Data[Chats::Language->value];
-    $this->LastSeen = $Data[Chats::LastSeen->value];
-    */
   }
 }
